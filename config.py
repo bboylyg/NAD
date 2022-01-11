54f6b20bd159ea36923f194e8ce7b1bb8c99bb66
Yige-Li <34542868+bboylyg@users.noreply.github.com>
2022-01-11 17:21:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ def get_arguments():
 
     # training hyper parameters
     parser.add_argument('--print_freq', type=int, default=50, help='frequency of showing training results on console')
-    parser.add_argument('--epochs', type=int, default=10, help='number of total epochs to run')
+    parser.add_argument('--epochs', type=int, default=20, help='number of total epochs to run')
     parser.add_argument('--batch_size', type=int, default=64, help='The size of batch')
     parser.add_argument('--lr', type=float, default=0.1, help='initial learning rate')
     parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
